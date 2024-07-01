d2f47e81ead88c590a51dfe4e33ba1f1539c637f
Haikun Xu <haikunxu1990@gmail.com>
2024-07-01 11:00:15 -0700

    improve the plotting functions

--- R/drawcells2.R
+++ R/drawcells2.R
@@ -3,11 +3,11 @@
 #'
 #' @export
 
-drawcells2 <- function(lats, lons, colseq, putnum = TRUE){
+drawcells2 <- function(lats, lons, dlat, dlon, colseq, putnum = TRUE){
   plot(lons, lats ,col=colseq,pch=15,cex=4,
-       ylim=c(-30,30),xlim=c(-150,-70), 
+       xlim=c(min(lons)-dlat,max(lons)+dlon),ylim=c(min(lats)-dlat,max(lats)+dlat), 
        ylab='latitude', xlab='longitude')#,main=paste('クラスター数 = ',kk))
   if(putnum) text(lons,lats,seq(1,length(lats), 1))
-  for(ss in seq(-150, -70, 5)) abline(v=ss, lty=2)
-  for(ss in seq(-30, 30, 5)) abline(h=ss, lty=2)
+  for(ss in seq(min(lons)-dlon/2, max(lons)+dlon/2, dlon)) abline(v=ss, lty=2)
+  for(ss in seq(min(lats)-dlat/2, max(lats)+dlat/2, dlat)) abline(h=ss, lty=2)
 }
