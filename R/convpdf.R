50dc044881cc018704b1990942df7ac2662a514d
Haikun Xu <haikunxu1990@gmail.com>
2024-08-19 21:16:38 -0700

    fix some bugs in the code

--- R/convpdf.R
+++ R/convpdf.R
@@ -13,6 +13,7 @@ convpdf<-function (origpdf, origx, convx){
   clen <- length(convx)
   convpdf <- vector('numeric', length=clen)
   convpdf[1]<- 0
+  
   for(cpos in 2:(clen-1)){
     opos <- max(sum(ifelse(origx < convx[cpos], 1, 0)),1) 
     cslope<- (origpdf[opos+1]-origpdf[opos])/(origx[opos+1]-origx[opos])
